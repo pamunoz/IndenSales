dd5fc611bf2e8e5a1ceac461bcd858882d544ff6
Pablo Munoz <pfariasmunoz@gmail.com>
2017-05-02 19:06:03 -0300

    Add a viewholder for the article adapter

--- app/src/main/java/com/pfariasmunoz/indensales/ui/adapters/ArticlesAdapter.java
+++ app/src/main/java/com/pfariasmunoz/indensales/ui/adapters/ArticlesAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -23,6 +24,16 @@ import java.util.List;
 
 public class ArticlesAdapter extends ArrayAdapter<Article> {
 
+    private static class ViewHolder {
+        ImageView articleImageView;
+        TextView articleDescriptionTextView;
+        TextView articlePriceTextView;
+        ImageButton addButton;
+        ImageButton subtractButton;
+        TextView articleAmountTextView;
+        TextView articleTotalPrice;
+    }
+
     // Constructor
     public ArticlesAdapter(@NonNull Context context,
                            @LayoutRes int resource,
@@ -33,24 +44,26 @@ public class ArticlesAdapter extends ArrayAdapter<Article> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        if (convertView == null) {
-            convertView = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.item_article, parent, false);
-        }
-
-        ImageView articleImageView = (ImageView) convertView.findViewById(R.id.iv_article_image);
-        TextView articleDescriptionTextView = (TextView) convertView.findViewById(R.id.tv_article_description);
-        TextView articlePriceTextView = (TextView) convertView.findViewById(R.id.tv_article_price);
-        ImageButton addButton = (ImageButton) convertView.findViewById(R.id.imb_up_arrow);
-        ImageButton subtractButton = (ImageButton) convertView.findViewById(R.id.imb_down_arrow);
-        TextView articleAmountTextView = (TextView) convertView.findViewById(R.id.tv_article_amount);
 
         Article article = getItem(position);
-
-        articleDescriptionTextView.setText(article.getDescripcion());
-        articlePriceTextView.setText(article.getPrecio());
-
-
-
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_article, parent, false);
+            viewHolder.articleImageView = (ImageView) convertView.findViewById(R.id.iv_article_image);
+            viewHolder.articleDescriptionTextView = (TextView) convertView.findViewById(R.id.tv_article_description);
+            viewHolder.articlePriceTextView = (TextView) convertView.findViewById(R.id.tv_article_price);
+            viewHolder.addButton = (ImageButton) convertView.findViewById(R.id.imb_up_arrow);
+            viewHolder.subtractButton = (ImageButton) convertView.findViewById(R.id.imb_down_arrow);
+            viewHolder.articleAmountTextView = (TextView) convertView.findViewById(R.id.tv_article_amount);
+            viewHolder.articleTotalPrice = (TextView) convertView.findViewById(R.id.tv_article_total_price);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.articleDescriptionTextView.setText(article.getDescripcion());
+        viewHolder.articlePriceTextView.setText(article.getPrecio());
         return convertView;
     }
 }
