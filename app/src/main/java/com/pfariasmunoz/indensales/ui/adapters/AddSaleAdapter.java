bae878b7d9b1c9c00d8a87d58478190ffc59103a
Pablo Munoz <pfariasmunoz@gmail.com>
2017-05-02 10:13:17 -0300

    Add adapter for the articles

--- app/src/main/java/com/pfariasmunoz/indensales/ui/adapters/AddSaleAdapter.java
+++ app/src/main/java/com/pfariasmunoz/indensales/ui/adapters/AddSaleAdapter.java
@@ -1,51 +1,63 @@
 package com.pfariasmunoz.indensales.ui.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.pfariasmunoz.indensales.R;
+import com.pfariasmunoz.indensales.data.models.Article;
+
+import java.util.List;
 
 /**
  * Created by Pablo Farias on 02-05-17.
  */
 
 public class AddSaleAdapter extends RecyclerView.Adapter<AddSaleAdapter.ArticleViewHolder> {
-    
+
+    private List<Article> mArticles;
+
+    public AddSaleAdapter(List<Article> articleList) {
+        this.mArticles = articleList;
+    }
 
     @Override
     public ArticleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_article, parent, false);
+        return new ArticleViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(ArticleViewHolder holder, int position) {
-
+        Article article = mArticles.get(position);
+        holder.mArticlePriceTextView.setText(article.getPrecio());
+        holder.mArticleDescriptionTextView.setText(article.getDescripcion());
+        holder.mArticleTotalPriceTextView.setText(article.getPrecio());
+        holder.mArticleAmountTextView.setText(String.valueOf(0));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mArticles.size();
     }
 
     public class ArticleViewHolder extends RecyclerView.ViewHolder {
-        TextView mClientNameTextView;
-        TextView mClientRutTextView;
-        TextView mClientAddressTextView;
-        TextView mArticlesAmountTextView;
-        TextView mTotalPriceTextView;
-        ToggleButton mCreateSaleToggleButton;
+        TextView mArticleDescriptionTextView;
+        TextView mArticlePriceTextView;
+        TextView mArticleAmountTextView;
+        TextView mArticleTotalPriceTextView;
+
 
         public ArticleViewHolder(View itemView) {
             super(itemView);
-            mClientNameTextView = (TextView) itemView.findViewById(R.id.tv_client_name);
-            mClientRutTextView = (TextView) itemView.findViewById(R.id.tv_client_rut);
-            mClientAddressTextView = (TextView) itemView.findViewById(R.id.tv_client_address);
-            mArticlesAmountTextView = (TextView) itemView.findViewById(R.id.tv_article_amount);
-            mTotalPriceTextView = (TextView) itemView.findViewById(R.id.tv_sale_total_price);
-            mCreateSaleToggleButton = (ToggleButton) itemView.findViewById(R.id.tb_togle_crear_venta);
+            mArticleDescriptionTextView = (TextView) itemView.findViewById(R.id.tv_article_description);
+            mArticlePriceTextView = (TextView) itemView.findViewById(R.id.tv_article_price);
+            mArticleAmountTextView = (TextView) itemView.findViewById(R.id.tv_article_amount);
+            mArticleTotalPriceTextView = (TextView) itemView.findViewById(R.id.tv_article_total_price);
         }
     }
 }
